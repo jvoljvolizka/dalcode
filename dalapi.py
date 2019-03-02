e45eb93e86bb6b0776d16c51e941b1eeb1d04fdf
Jvol <damnedprotoss@gmail.com>
2019-03-03 02:34:01 +0300

    dalapi

---
+++ dalapi.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, send_file
+from werkzeug import secure_filename
+import subprocess
+app = Flask(__name__)
+
+@app.route('/')
+def uploade_file():
+   return render_template('index.html'),418
+
+@app.route('/encode', methods = ['GET', 'POST'])
+def encode_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(secure_filename(f.filename))
+      out = subprocess.check_output("./dalencode.py " + f.filename + " > encoded.dal", shell=True)
+      subprocess.call("rm ./" +  f.filename , shell=True)
+      return send_file("encoded.dal" , attachment_filename='encoded.dal')
+
+@app.route('/decode', methods = ['GET', 'POST'])
+def decode_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(secure_filename(f.filename))
+      out = subprocess.check_output("./daldecode.py " + f.filename + " decoded.dal", shell=True)
+      subprocess.call("rm ./" +  f.filename , shell=True)
+      return send_file("decoded.dal" , attachment_filename='decoded.dal')
+if __name__ == '__main__':
+   app.run(host="0.0.0.0", debug = True)
